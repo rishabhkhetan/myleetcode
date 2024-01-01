535402ea30a4ecbd16082ca39269a9f8f498f258
rishabhkhetan <rishabh.khetan@outlook.com>
2024-01-01 12:19:21 +0530

    redistribute characters to make all strings equal

---
+++ Redistribute characters to make all strings equal.js
@@ -0,0 +1,47 @@
+/*You are given an array of strings words (0-indexed).
+
+In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
+
+Return true if you can make every string in words equal using any number of operations, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: words = ["abc","aabc","bc"]
+Output: true
+Explanation: Move the first 'a' in words[1] to the front of words[2],
+to make words[1] = "abc" and words[2] = "abc".
+All the strings are now equal to "abc", so return true.
+Example 2:
+
+Input: words = ["ab","a"]
+Output: false
+Explanation: It is impossible to make all the strings equal using the operation.
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of lowercase English letters.*/ 
+
+
+
+var makeEqual = function(words) {
+    const frequency = new Array(26).fill(0);
+
+    for (const word of words) {
+        for (const c of word) {
+            frequency[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+            }
+        }
+
+        for (let i = 0; i < 26; i++) {
+            if (frequency[i] % words.length !== 0) {
+            return false;
+            }
+        }
+
+    return true;
+}
